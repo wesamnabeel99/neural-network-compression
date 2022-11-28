40694c4d8a6baf1e656ab148a02a5c0060d80d4d
Wesam Nabeel <wesamezio@gmail.com>
2022-11-28 13:44:26 +0300

    gradient decent implementation

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as matplot
 import numpy
 import pandas
-from lib.sigmoid import sigmoid
 
-from utils.neural_network_utils import calculate_neural_network_output
+from utils.neural_network_utils import test_neural_network
 
 test_file_path = "dataset/mnist_test.csv"
 train_file_path = "dataset/mnist_train.csv"
@@ -14,21 +13,20 @@ images, ground_truth = data_train[:, 1:], data_train[:, 0]
 
 sample_image = images[[0], :].reshape(28, 28)
 
-
 matplot.imshow(sample_image)
 matplot.savefig("temp/sample.png")
 
 LABELS_SHAPE = 0
 PIXELS_SHAPE = 1
 
-labels = images.shape[LABELS_SHAPE]
+input_neuron = images.shape[PIXELS_SHAPE]
 
+labels = images.shape[LABELS_SHAPE]
 hidden_neurons = 100
 output_neurons = 10
 
-hidden_weights = numpy.random.uniform(-1, 1, size=(hidden_neurons, sample_image.size)),
+hidden_weights = numpy.random.uniform(-1, 1, size=(hidden_neurons, input_neuron))
 output_weights = numpy.random.uniform(-1, 1, size=(output_neurons, hidden_neurons))
-
 
 evaluation = 0
 
@@ -38,13 +36,23 @@ error = 0
 for i in range(labels):
     input_image = images[[i], :]
 
-    hidden_output = calculate_neuron_output(input_neurons, hidden_weights)
-    return calculate_neuron_output(hidden_output, output_weights)
+    output_output = test_neural_network(
+    input_neurons=input_image,
+    hidden_weights=hidden_weights,
+    output_weights=output_weights,
+    )
 
-    error = numpy.sum((output_output - one_hot_encoding)** 2)
-    loss_function = numpy.subtract(output_output,one_hot_encoding) * ou
+    error = numpy.sum((output_output - one_hot_encoding)**2)
+
+    cost = error / 2
+
+    cost_function = numpy.multiply(numpy.subtract(output_output,one_hot_encoding),hidden_weights)
+
+    hidden_weights = numpy.subtract(hidden_weights, cost_function * alpha)
+
     winning_class = numpy.argmax(output_output)
     evaluation += winning_class == ground_truth[i]
+
 accuracy = evaluation / labels * 100
 print(evaluation / labels * 100)
 print(error)
--- utils/neural_network_utils.py
+++ utils/neural_network_utils.py
@@ -1,8 +1,9 @@
 import numpy
+
 from lib.sigmoid import sigmoid
 
 
-def calculate_neural_network_output(input_neurons,hidden_weights, output_weights):
+def test_neural_network(input_neurons, hidden_weights, output_weights):
     hidden_output = calculate_neuron_output(input_neurons, hidden_weights)
     return calculate_neuron_output(hidden_output, output_weights)
 
