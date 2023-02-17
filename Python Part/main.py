49c856aa52b813a67dbfae73a671ac417ae4bbc0
Wesam Nabeel <wesamezio@gmail.com>
2023-02-17 05:28:40 +0300

    improve code by using object-oriented programming

--- Python Part/main.py
+++ Python Part/main.py
@@ -7,16 +7,21 @@ Descripation: This is to implement multi neural network
 import matplotlib.pyplot as plt
 import numpy as np
 
-from lib.dnn_layers import dnn_layers
-from lib.sigmoid import sigmoid
+from lib.deepneuralnetworklayers import DeepNeuralNetworkLayers
 from utils.read_mnist_data import read_mnist_data
 
-dnn = dnn_layers()
-
 # extracting the image
-images_train, labels_train = read_mnist_data("dataset/mnist_train.csv",1000)
-images_test, labels_test = read_mnist_data("dataset/mnist_test.csv",100)
+images_train, labels_train = read_mnist_data("dataset/mnist_train.csv", 1000)
+images_test, labels_test = read_mnist_data("dataset/mnist_test.csv", 100)
 
+# Defining the hyperparameter
+input_neurons = images_train.shape[1]
+hidden_neurons = 100
+output_neurons = 10
+alpha = 0.5
+epoch_size = 50
+
+dnn = DeepNeuralNetworkLayers(input_neurons, hidden_neurons, output_neurons)
 
 one_hot_encoding = np.eye(10)[labels_train.astype(int)]
 
@@ -35,67 +40,16 @@ plt.figure(num=1)
 plt.imshow(sample3)
 plt.savefig('temp/meow.png')
 
-# Defining the hyperparameter
-input_neurons = images_train.shape[1]
-hidden_neurons = 100
-output_neurons = 10
-images_to_be_trained = images_train.shape[0]
-images_to_be_tested = images_test.shape[0]
-alpha = 0.5
-epoch_size = 50
+accuracy_train = dnn.train(epoch_size=epoch_size, alpha=alpha, images=images_train, labels=labels_train)
 
-# Giving the layers there weights
-hidden_weights = np.random.uniform(-1, 1, size=(hidden_neurons, input_neurons))
-output_weights = np.random.uniform(-1, 1, size=(output_neurons, hidden_neurons))
+accuracy_test = dnn.test(images=images_test, labels=labels_test)
 
-acc_tr = []
-acc_tes = []
-for epoch in range(epoch_size):
-    accum_acc = 0
-    for i in range(images_to_be_trained):
-        hidden_logit = np.dot(images_train[i, :], hidden_weights.T)
-        hidden_output = sigmoid(hidden_logit)
-
-        output_logit = np.dot(hidden_output, output_weights.T)
-        final_output = sigmoid(output_logit)
-
-        # Training part
-        error = final_output - one_hot_encoding[i]
-        output_weights -= alpha * np.outer(error, hidden_output)
-        # network evaluation
-        winning_class = np.argmax(final_output)
-
-        # compare the  winning class with the ground truth
-        evaluation = 1.0 * (winning_class == labels_train[i])
-        accum_acc += evaluation
-
-    # final result
-    acc_tr.append((accum_acc / len(labels_train)) * 100)
-
-    accum_acc = 0
-    for i in range(images_to_be_tested):
-        hidden_logit = np.dot(images_test[i, :], hidden_weights.T)
-        hidden_output = sigmoid(hidden_logit)
-
-        output_logit = np.dot(hidden_output, output_weights.T)
-        final_output = sigmoid(output_logit)
-
-        winning_class = np.argmax(final_output)
-        # compare the  winning class with the ground truth
-        evaluation = 1.0 * (winning_class == labels_test[i])
-        accum_acc += evaluation
-
-    acc_tes.append((accum_acc / len(labels_test) * 100))
-
-# Testing
-
-
-print("final training accuracy = %.3f" % acc_tr[-1])
-print("final training accuracy = %.3f" % acc_tes[-1])
+print("final training accuracy = %.3f" % accuracy_train[-1])
+print("final test accuracy = %.3f" % accuracy_test[-1])
 
 plt.figure(2)
-plot = plt.plot(acc_tr)
-plt.plot(acc_tes)
+plot = plt.plot(accuracy_train)
+plt.plot(accuracy_test)
 plt.grid()
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
--- Python Part/utils/neural_network_utils.py
+++ Python Part/utils/neural_network_utils.py
@@ -1,12 +1,12 @@
 import numpy
 from lib.sigmoid import sigmoid
 
-# Change the input no
+
 def test_neural_network(input_neurons, hidden_weights, output_weights):
     hidden_output = calculate_neuron_output(input_neurons, hidden_weights)
     return calculate_neuron_output(hidden_output, output_weights)
 
 
 def calculate_neuron_output(neuron, weight):
-    logit = numpy.dot(neuron,weight.T)
+    logit = numpy.dot(neuron, weight.T)
     return sigmoid(logit)
