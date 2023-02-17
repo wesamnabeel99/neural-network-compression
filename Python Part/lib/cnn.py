d2809d1cdba4970ac113f2f4ac33e16149bf24a7
A99-turing <abdullah.majed1802@coeng.uobaghdad.edu.iq>
2023-02-17 19:50:22 +0300

    adding convolution and pooling layers

--- Python Part/lib/cnn.py
+++ Python Part/lib/cnn.py
@@ -1,16 +1,32 @@
 import numpy as np
 
 
-class cnn:
+class cnn_layers():
+    def __int__(self, x):
+        self.x = x
 
     def convolve(self, image, kernel):
-        output = np.zeros([100, 676], dtype=float, order='C')
-        image_2d = image[:, :].reshape(28, -1)
-        for i in range(image.shape[0]):
-            for j in range(26):
-                for k in range(26):
-                    for m in range(3):
-                        for n in range(3):
-                            output[j][k] += image_2d[j + m][k + n] * kernel[m][n]
+        image_height, image_width = image.shape
+        kernel_height, kernel_width = kernel.shape
 
+        output = np.zeros((image_height - kernel_height + 1, image_width - kernel_width + 1), dtype="float32")
+        for i in range(image_height - kernel_height + 1):
+            for j in range(image_width - kernel_width + 1):
+                output[i, j] = (image[i:i + kernel_height, j:j + kernel_width] * kernel).sum()
         return output
+
+    def pool(self, convolved_image):
+        stride = 2
+        pool_output = []
+        for i in range(0, convolved_image.shape[0], stride):
+            row = []
+            for j in range(0, convolved_image.shape[1], stride):
+                row.append(np.amax(convolved_image[i:i + stride, j:j + stride]))
+            pool_output.append(row)
+        return np.array(pool_output)
+
+
+
+
+
+
