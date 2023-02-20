811c23ca82eda8686bc7d6b20a1e22a810e1b7bc
Wesam Nabeel <wesamezio@gmail.com>
2023-02-20 05:31:13 +0300

    create models in oop style

--- Python Part/main.py
+++ Python Part/main.py
@@ -7,24 +7,29 @@ import winsound
 
 from utils import constants
 from utils.read_mnist_data import read_mnist_data
-from models import *
+from network_models import *
 
 images_train, labels_train = read_mnist_data(constants.MNIST_TRAIN_FILEPATH, constants.TRAIN_SAMPLE_SIZE)
 images_test, labels_test = read_mnist_data(constants.MNIST_TEST_FILEPATH, constants.TEST_SAMPLE_SIZE)
 
-model_one(images_train,labels_train,images_test,labels_test)
+network_models = network_models(
+    images_train=images_train, labels_train=labels_train, images_test=images_test, labels_test=labels_test,
+    n_kernels=3, epoch=10, alpha=0.1, hidden=100
+)
+
+network_models.model_one()
 winsound.Beep(440, 1000)
 
-model_two(images_train,labels_train,images_test,labels_test)
+network_models.model_two()
 winsound.Beep(440, 1000)
 
-model_three(images_train,labels_train,images_test,labels_test)
+network_models.model_three()
 winsound.Beep(440, 1000)
 
-model_four(images_train,labels_train,images_test,labels_test)
+network_models.model_four()
 winsound.Beep(440, 1000)
 
-model_five(images_test,labels_test)
+network_models.model_five()
 winsound.Beep(440, 1000)
 
 winsound.Beep(800, 500)
---
+++ Python Part/network_models.py
@@ -0,0 +1,130 @@
+from lib.cnn import cnn_layers
+from lib.dnn_layers import dnn_layers
+from utils.image_helper import *
+
+
+class network_models:
+    def __init__(self, images_train, labels_train, images_test, labels_test, n_kernels, epoch, alpha, hidden):
+        self.images_train = images_train
+        self.labels_train = labels_train
+        self.images_test = images_test
+        self.labels_test = labels_test
+        self.cnn = cnn_layers(n_kernels=n_kernels)
+        self.epoch = epoch
+        self.alpha = alpha
+        self.hidden = hidden
+
+    def model_one(self):
+        model_name = "cnn_pooling_dnn"
+
+        print("\n\n")
+        print(f"=======---------model (({model_name})) has started---------=======")
+        print("\n\n")
+        images_train = reshape_all_images(self.images_train)
+        images_test = reshape_all_images(self.images_test)
+
+        images_train = self.cnn.convolve(images_train)
+        images_train = self.cnn.pool(images_train)
+        images_train = flatten_all_images(images_train)
+
+        images_test = self.cnn.convolve(images_test)
+        images_test = self.cnn.pool(images_test)
+        images_test = flatten_all_images(images_test)
+
+        dnn = dnn_layers(input_neurons=images_train.shape[1], hidden_neurons=self.hidden, output_neurons=10)
+
+        dnn.evaluate_model(
+            epoch_size=self.epoch, alpha=self.alpha, images_test=images_test,
+            labels_test=self.labels_test, images_train=images_train, labels_train=self.labels_train,
+            model_name=model_name
+        )
+        print("\n\n")
+        print(f"=======---------model (({model_name})) finished---------=======")
+        print("\n\n")
+
+    def model_two(self):
+        model_name = "cnn_dnn"
+        print("\n\n")
+        print(f"=======---------model (({model_name})) has started---------=======")
+        print("\n\n")
+
+        images_train = reshape_all_images(self.images_train)
+        images_test = reshape_all_images(self.images_test)
+
+        images_train = self.cnn.convolve(images_train)
+        images_train = flatten_all_images(images_train)
+
+        images_test = self.cnn.convolve(images_test)
+        images_test = flatten_all_images(images_test)
+
+        dnn = dnn_layers(input_neurons=images_train.shape[1], hidden_neurons=self.hidden, output_neurons=10)
+
+        dnn.evaluate_model(
+            epoch_size=self.epoch, alpha=self.alpha, images_test=images_test,
+            labels_test=self.labels_test, images_train=images_train, labels_train=self.labels_train,
+            model_name=model_name
+        )
+        print("\n\n")
+        print(f"=======---------model (({model_name})) finished---------=======")
+        print("\n\n")
+
+    def model_three(self):
+        model_name = "dnn"
+        print("\n\n")
+        print(f"=======---------model (({model_name})) has started---------=======")
+        print("\n\n")
+        dnn = dnn_layers(input_neurons=self.images_train.shape[1], hidden_neurons=self.hidden, output_neurons=10)
+
+        dnn.evaluate_model(
+            epoch_size=self.epoch, alpha=self.alpha, images_test=self.images_test,
+            labels_test=self.labels_test, images_train=self.images_train, labels_train=self.labels_train,
+            model_name="dnn"
+        )
+        print("\n\n")
+        print(f"=======---------model (({model_name})) finished---------=======")
+        print("\n\n")
+
+    def model_four(self):
+        model_name = "pooling_dnn"
+        print("\n\n")
+        print(f"=======---------model (({model_name})) has started---------=======")
+        print("\n\n")
+
+        images_train = reshape_all_images(self.images_train)
+        images_test = reshape_all_images(self.images_test)
+
+        images_train = self.cnn.pool(images_train)
+        images_train = flatten_all_images(images_train)
+
+        images_test = self.cnn.pool(images_test)
+        images_test = flatten_all_images(images_test)
+
+        dnn = dnn_layers(input_neurons=images_train.shape[1], hidden_neurons=self.hidden, output_neurons=10)
+
+        dnn.evaluate_model(
+            epoch_size=self.epoch, alpha=self.alpha, images_test=images_test,
+            labels_test=self.labels_test, images_train=images_train, labels_train=self.labels_train,
+            model_name=model_name
+        )
+        print("\n\n")
+        print(f"=======---------model (({model_name})) finished---------=======")
+        print("\n\n")
+
+    def model_five(self):
+        model_name = "cnn_dnn_forward"
+        print("\n\n")
+        print(f"=======---------model (({model_name})) has started---------=======")
+        print("\n\n")
+
+        images_test = reshape_all_images(self.images_test)
+
+        images_test = self.cnn.convolve(images_test)
+        images_test = flatten_all_images(images_test)
+
+        dnn = dnn_layers(input_neurons=images_test.shape[1], hidden_neurons=self.hidden, output_neurons=10)
+
+        accuracy = dnn.forward(images_test, self.labels_test)
+        print(f"model {model_name} accuracy: {accuracy}")
+        print("\n\n")
+        print(f"=======---------model (({model_name})) finished---------=======")
+        print("\n\n")
